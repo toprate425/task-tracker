09272edd82f2eb7407462e4f0cb98692c3866bca
toprate425 <96052595+toprate425@users.noreply.github.com>
2022-07-06 13:12:37 -0700

    deploy taskTracker

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {Task} from '../../Task';
+import { TaskService } from 'src/app/services/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    })
+  }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => {
+      this.tasks = this.tasks.filter(t => t.id !== task.id);
+      console.log('task deleted:',task)
+    })
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTask(task).subscribe(() => {
+      console.log('task updated:',task);
+    });
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe(() => {
+      console.log('task added:',task);
+      this.tasks.push(task);
+    });
+  }
+
+}
